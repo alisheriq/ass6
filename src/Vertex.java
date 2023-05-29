3ba874495ad42441908c718ebe0c2dd93468ebb9
admin <eeoneguifeed@gmail.com>
2023-05-29 11:25:33 +0600

    Vertex class update

--- src/Vertex.java
+++ src/Vertex.java
@@ -1,2 +1,14 @@
-public class Vertex {
+import java.util.HashMap;
+import java.util.Map;
+
+public class Vertex <V>{
+    private V data;
+    private Map<Vertex<V>, Double> adjacentVertices;
+    private double distance;
+
+    public Vertex(V data) {
+        this.data = data;
+        this.adjacentVertices = new HashMap<>();
+        this.distance = Double.POSITIVE_INFINITY;
+    }
 }
