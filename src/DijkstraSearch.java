2bedd9c82693862dfc0ce0d548e82a4fa6e76a3c
admin <eeoneguifeed@gmail.com>
2023-05-29 11:49:22 +0600

    DijkstraSearch class updated

--- src/DijkstraSearch.java
+++ src/DijkstraSearch.java
@@ -1,2 +1,7 @@
-public class DijkstraSearch <V>{
+public class DijkstraSearch <V> implements Search<V>{
+    private WeightedGraph<V> graph;
+
+    public DijkstraSearch(WeightedGraph<V> graph) {
+        this.graph = graph;
+    }
 }
