9a26264a001a1c232cf21afaad0f66e2ffe7c745
admin <eeoneguifeed@gmail.com>
2023-05-29 11:50:12 +0600

    DijkstraSearch class updated(1 method implemented)

--- src/DijkstraSearch.java
+++ src/DijkstraSearch.java
@@ -1,7 +1,47 @@
+import java.util.*;
 public class DijkstraSearch <V> implements Search<V>{
     private WeightedGraph<V> graph;
 
     public DijkstraSearch(WeightedGraph<V> graph) {
         this.graph = graph;
+    }
+    @Override
+    public List<V> findPath(Vertex<V> source, Vertex<V> destination) {
+        PriorityQueue<Vertex<V>> queue = new PriorityQueue<>(Comparator.comparingDouble(Vertex<V>::getDistance));
+        Map<Vertex<V>, Double> distances = new HashMap<>();
+        Map<Vertex<V>, Vertex<V>> parentMap = new HashMap<>();
+
+        for (Vertex<V> vertex : graph.getVertices()) {
+            if (vertex.equals(source)) {
+                vertex.setDistance(0.0);
+            } else {
+                vertex.setDistance(Double.POSITIVE_INFINITY);
+            }
+            distances.put(vertex, vertex.getDistance());
+            queue.add(vertex);
+        }
+
+        while (!queue.isEmpty()) {
+            Vertex<V> current = queue.poll();
+
+            if (current.equals(destination)) {
+                return constructPath(parentMap, destination);
+            }
+
+            for (WeightedGraph.WeightedEdge<V> edge : graph.getEdges(current)) {
+                Vertex<V> neighbor = (Vertex<V>) edge.getDestination();
+                double newDistance = current.getDistance() + edge.getWeight();
+
+                if (newDistance < neighbor.getDistance()) {
+                    queue.remove(neighbor);
+                    neighbor.setDistance(newDistance);
+                    distances.put(neighbor, newDistance);
+                    parentMap.put(neighbor, current);
+                    queue.add(neighbor);
+                }
+            }
+        }
+
+        return Collections.emptyList(); // No path found
     }
 }
