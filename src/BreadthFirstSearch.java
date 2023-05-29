0336b841a2015fa7b8465b0dd46a1cbcafc7dd20
admin <eeoneguifeed@gmail.com>
2023-05-29 12:13:09 +0600

    Comments added(2)

--- src/BreadthFirstSearch.java
+++ src/BreadthFirstSearch.java
@@ -1,47 +1,49 @@
 import java.util.*;
 
-public class BreadthFirstSearch <V> implements Search<V> {
+public class BreadthFirstSearch<V> implements Search<V> {
     private WeightedGraph<V> graph;
 
     public BreadthFirstSearch(WeightedGraph<V> graph) {
         this.graph = graph;
     }
+
     @Override
     public List<V> findPath(Vertex<V> source, Vertex<V> destination) {
-        Queue<Vertex<V>> queue = new LinkedList<>();
-        Map<Vertex<V>, Vertex<V>> parentMap = new HashMap<>();
+        Queue<Vertex<V>> queue = new LinkedList<>();  // Queue to perform breadth-first search
+        Map<Vertex<V>, Vertex<V>> parentMap = new HashMap<>();  // Map to store the parent vertices for constructing the path
 
-        queue.add(source);
-        parentMap.put(source, null);
+        queue.add(source);  // Start the search from the source vertex
+        parentMap.put(source, null);  // Set the parent of the source vertex as null
 
         while (!queue.isEmpty()) {
-            Vertex<V> current = queue.poll();
+            Vertex<V> current = queue.poll();  // Take the next vertex from the queue
 
             if (current.equals(destination)) {
-                return constructPath(parentMap, destination);
+                return constructPath(parentMap, destination);  // If the destination vertex is found, construct and return the path
             }
 
             for (WeightedGraph.WeightedEdge<V> edge : graph.getEdges(current)) {
-                Vertex<V> neighbor = (Vertex<V>) edge.getDestination();
+                Vertex<V> neighbor = (Vertex<V>) edge.getDestination();  // Get the neighboring vertex
+
                 if (!parentMap.containsKey(neighbor)) {
-                    queue.add(neighbor);
-                    parentMap.put(neighbor, current);
+                    queue.add(neighbor);  // Add the neighbor to the queue for further exploration
+                    parentMap.put(neighbor, current);  // Set the current vertex as the parent of the neighbor
                 }
             }
         }
 
-        return Collections.emptyList(); // No path found
+        return Collections.emptyList();  // If no path is found, return an empty list
     }
+
     private List<V> constructPath(Map<Vertex<V>, Vertex<V>> parentMap, Vertex<V> destination) {
         List<V> path = new LinkedList<>();
         Vertex<V> current = destination;
 
         while (current != null) {
-            path.add(0, current.getData());
-            current = parentMap.get(current);
+            path.add(0, current.getData());  // Add the data of each vertex to the path list in reverse order
+            current = parentMap.get(current);  // Move to the parent vertex
         }
 
         return path;
     }
-
 }
--- src/DijkstraSearch.java
+++ src/DijkstraSearch.java
@@ -1,10 +1,12 @@
 import java.util.*;
-public class DijkstraSearch <V> implements Search<V>{
+
+public class DijkstraSearch<V> implements Search<V> {
     private WeightedGraph<V> graph;
 
     public DijkstraSearch(WeightedGraph<V> graph) {
         this.graph = graph;
     }
+
     @Override
     public List<V> findPath(Vertex<V> source, Vertex<V> destination) {
         PriorityQueue<Vertex<V>> queue = new PriorityQueue<>(Comparator.comparingDouble(Vertex<V>::getDistance));
@@ -13,44 +15,45 @@ public class DijkstraSearch <V> implements Search<V>{
 
         for (Vertex<V> vertex : graph.getVertices()) {
             if (vertex.equals(source)) {
-                vertex.setDistance(0.0);
+                vertex.setDistance(0.0);  // Set the distance of the source vertex to 0
             } else {
-                vertex.setDistance(Double.POSITIVE_INFINITY);
+                vertex.setDistance(Double.POSITIVE_INFINITY);  // Set the distance of other vertices to infinity
             }
-            distances.put(vertex, vertex.getDistance());
-            queue.add(vertex);
+            distances.put(vertex, vertex.getDistance());  // Store the distances in the map
+            queue.add(vertex);  // Add the vertices to the priority queue
         }
 
         while (!queue.isEmpty()) {
-            Vertex<V> current = queue.poll();
+            Vertex<V> current = queue.poll();  // Get the vertex with the smallest distance from the queue
 
             if (current.equals(destination)) {
-                return constructPath(parentMap, destination);
+                return constructPath(parentMap, destination);  // If the destination vertex is found, construct and return the path
             }
 
             for (WeightedGraph.WeightedEdge<V> edge : graph.getEdges(current)) {
-                Vertex<V> neighbor = (Vertex<V>) edge.getDestination();
-                double newDistance = current.getDistance() + edge.getWeight();
+                Vertex<V> neighbor = (Vertex<V>) edge.getDestination();  // Get the neighboring vertex
+                double newDistance = current.getDistance() + edge.getWeight();  // Calculate the new distance
 
                 if (newDistance < neighbor.getDistance()) {
-                    queue.remove(neighbor);
-                    neighbor.setDistance(newDistance);
-                    distances.put(neighbor, newDistance);
-                    parentMap.put(neighbor, current);
-                    queue.add(neighbor);
+                    queue.remove(neighbor);  // Remove the neighbor from the queue to update its distance
+                    neighbor.setDistance(newDistance);  // Update the distance of the neighbor
+                    distances.put(neighbor, newDistance);  // Update the distance in the map
+                    parentMap.put(neighbor, current);  // Update the parent vertex in the parent map
+                    queue.add(neighbor);  // Add the neighbor back to the queue with the updated distance
                 }
             }
         }
 
-        return Collections.emptyList(); // No path found
+        return Collections.emptyList();  // If no path is found, return an empty list
     }
+
     private List<V> constructPath(Map<Vertex<V>, Vertex<V>> parentMap, Vertex<V> destination) {
         List<V> path = new LinkedList<>();
         Vertex<V> current = destination;
 
         while (current != null) {
-            path.add(0, current.getData());
-            current = parentMap.get(current);
+            path.add(0, current.getData());  // Add the data of each vertex to the path list in reverse order
+            current = parentMap.get(current);  // Move to the parent vertex
         }
 
         return path;
