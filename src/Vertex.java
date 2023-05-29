436025b5d1948573c668bcd420e20f9fb254858b
admin <eeoneguifeed@gmail.com>
2023-05-29 12:11:15 +0600

    Comments added

--- src/Vertex.java
+++ src/Vertex.java
@@ -2,31 +2,33 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class Vertex <V>{
-    private V data;
-    private Map<Vertex<V>, Double> adjacentVertices;
-    private double distance;
+    private V data;  // Stores the data associated with the vertex
+    private Map<Vertex<V>, Double> adjacentVertices;  // Stores the adjacent vertices and their corresponding weights
+    private double distance;  // Stores the distance of the vertex from a source vertex
 
     public Vertex(V data) {
         this.data = data;
-        this.adjacentVertices = new HashMap<>();
-        this.distance = Double.POSITIVE_INFINITY;
+        this.adjacentVertices = new HashMap<>();  // Initializes the map to store adjacent vertices
+        this.distance = Double.POSITIVE_INFINITY;  // Sets the initial distance to infinity
     }
+
     public V getData() {
         return data;
     }
 
     public void addAdjacentVertex(Vertex<V> destination, double weight) {
-        adjacentVertices.put(destination, weight);
+        adjacentVertices.put(destination, weight);  // Adds an adjacent vertex with its weight to the map
     }
+
     public Map<Vertex<V>, Double> getAdjacentVertices() {
-        return adjacentVertices;
+        return adjacentVertices;  // Returns the map of adjacent vertices
     }
 
     public void setDistance(double distance) {
-        this.distance = distance;
+        this.distance = distance;  // Sets the distance of the vertex from a source vertex
     }
 
     public double getDistance() {
-        return distance;
+        return distance;  // Returns the distance of the vertex from a source vertex
     }
 }
--- src/WeightedGraph.java
+++ src/WeightedGraph.java
@@ -3,21 +3,23 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-public class WeightedGraph <V>{
-    private Map<Vertex<V>, List<WeightedEdge<V>>> map;
+public class WeightedGraph<V> {
+    private Map<Vertex<V>, List<WeightedEdge<V>>> map;  // Stores the vertices and their corresponding weighted edges
+
     public WeightedGraph() {
-        this.map = new HashMap<>();
+        this.map = new HashMap<>();  // Initializes the map to store the vertices and edges
     }
+
     public void addVertex(Vertex<V> vertex) {
-        map.put(vertex, new ArrayList<>());
+        map.put(vertex, new ArrayList<>());  // Adds a vertex to the graph with an empty list of edges
     }
 
     public void addEdge(Vertex<V> source, Vertex<V> destination, double weight) {
         validateVertex(source);
         validateVertex(destination);
 
-        List<WeightedEdge<V>> edges = map.get(source);
-        edges.add(new WeightedEdge<>(source, destination, weight));
+        List<WeightedEdge<V>> edges = map.get(source);  // Retrieves the list of edges for the source vertex
+        edges.add(new WeightedEdge<>(source, destination, weight));  // Creates a new weighted edge and adds it to the list
     }
 
     private void validateVertex(Vertex<V> vertex) {
@@ -25,26 +27,28 @@ public class WeightedGraph <V>{
             throw new IllegalArgumentException("Vertex " + vertex + " is not part of the graph");
         }
     }
+
     public List<WeightedEdge<V>> getEdges() {
         List<WeightedEdge<V>> allEdges = new ArrayList<>();
         for (List<WeightedEdge<V>> edges : map.values()) {
-            allEdges.addAll(edges);
+            allEdges.addAll(edges);  // Collects all the weighted edges from each vertex and adds them to the list
         }
         return allEdges;
     }
 
     public List<WeightedEdge<V>> getEdges(Vertex<V> vertex) {
         validateVertex(vertex);
-        return map.get(vertex);
+        return map.get(vertex);  // Retrieves the list of weighted edges for the specified vertex
     }
 
     public List<Vertex<V>> getVertices() {
-        return new ArrayList<>(map.keySet());
+        return new ArrayList<>(map.keySet());  // Returns a list of all vertices in the graph
     }
+
     public static class WeightedEdge<V> {
-        private Vertex<V> source;
-        private Vertex<V> destination;
-        private double weight;
+        private Vertex<V> source;  // The source vertex of the edge
+        private Vertex<V> destination;  // The destination vertex of the edge
+        private double weight;  // The weight of the edge
 
         public WeightedEdge(Vertex<V> source, Vertex<V> destination, double weight) {
             this.source = source;
@@ -53,15 +57,15 @@ public class WeightedGraph <V>{
         }
 
         public Vertex<V> getSource() {
-            return source;
+            return source;  // Returns the source vertex of the edge
         }
 
         public Vertex<V> getDestination() {
-            return destination;
+            return destination;  // Returns the destination vertex of the edge
         }
 
         public double getWeight() {
-            return weight;
+            return weight;  // Returns the weight of the edge
         }
     }
 }
